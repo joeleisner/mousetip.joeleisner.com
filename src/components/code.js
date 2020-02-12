bcc823195c891bd9081bdf807ab77952fbad9866
Joel Eisner <jeisner93@gmail.com>
2020-02-11 20:22:06 -0500

    Updated the Code React component for the following:
    1. Safely set the inner HTML of a code example in order to access its
       content via regular DOM methods (e.g. querySelectorAll).
    2. Added two new props: mousetip and settings. The "mousetip" prop tells
       the component whether to run Pure JS Mousetip or not (boolean). The
       "settings" prop will be passed into the MouseTip constructor when
       initialized (object). Both are set to true and {} by default
       respectively.
    
    In addition, the mousetip example on the "Getting started" page has been
    updated to use the proper attribute syntax for v3.0.0 (e.g. mousetip:msg
    instead of mousetip-msg).

--- src/components/code.js
+++ src/components/code.js
@@ -4,6 +4,8 @@ import SyntaxHighlighter from 'react-syntax-highlighter';
 import Dark from 'react-syntax-highlighter/dist/esm/styles/hljs/a11y-dark';
 import { CopyToClipboard } from 'react-copy-to-clipboard';
 
+import MouseTip from 'purejs-mousetip';
+
 import '../sass/components/code.sass';
 
 class Code extends React.Component {
@@ -17,14 +19,13 @@ class Code extends React.Component {
     };
 
     convertChildrenIntoCodeExample() {
-        const example = (
-            <div
-                className="code__example"
-                dangerouslySetInnerHTML={{ __html: this.props.children }}
-            ></div>
-        );
+        this.refs.example.innerHTML = this.props.children;
 
-        this.setState({ example });
+        if (!this.props.mousetip) return;
+    
+        const mouseTip = new MouseTip(this.props.settings);
+
+        mouseTip.start(Array.from(this.refs.example.querySelectorAll(`[${ mouseTip.selector.full }], [${ mouseTip.selector.short }]`)));
     }
 
     componentDidMount() {
@@ -39,9 +40,13 @@ class Code extends React.Component {
 
     render() {
         const { language, example, children, ...props } = this.props;
+
+        delete props.mousetip;
+        delete props.settings;
+
         return (
             <div className="code">
-                {example ? this.state.example : ''}
+                {example ? <div className="code__example" ref="example"></div> : ''}
                 <div className="code__snippet">
                     <CopyToClipboard
                         text={children}
@@ -69,12 +74,16 @@ class Code extends React.Component {
 Code.propTypes = {
     language: PropTypes.string,
     example: PropTypes.bool,
+    mousetip: PropTypes.bool,
+    settings: PropTypes.object,
     children: PropTypes.string.isRequired
 };
 
 Code.defaultProps = {
     language: 'html',
-    example: true
+    example: true,
+    mousetip: true,
+    settings: {}
 };
 
 export default Code;
