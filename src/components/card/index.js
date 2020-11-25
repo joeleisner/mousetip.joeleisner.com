4a665794a7cbbb06f10e961222e5bd973d70fd8c
Joel Eisner <jeisner93@gmail.com>
2020-11-25 17:15:56 -0500

    Homepage examples
    Updated the homepage, and subsequently the card component, to show some
    working examples of Pure JS Mousetip in action.

--- src/components/card/index.js
+++ src/components/card/index.js
@@ -2,11 +2,17 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 import Icon from '../icon';
+import MouseTip from 'purejs-mousetip';
 
 import './card.scss';
 
-const Card = ({ icon, title, children, ...props }) => {
-    function createTitle(icon, title) {
+class Card extends React.Component {
+    constructor(props) {
+        super(props);
+        this.card = React.createRef();
+    }
+
+    createTitle(icon, title) {
         if (!icon && !title) return '';
 
         let className = ['card__title'];
@@ -23,18 +29,38 @@ const Card = ({ icon, title, children, ...props }) => {
         );
     }
 
-    return (
-        <div className="card" {...props}>
-            {createTitle(icon, title)}
-            <div className="card__description">{children}</div>
-        </div>
-    );
-};
+    mousetip() {
+        const settings = typeof this.props.mousetip === 'boolean' ? {} : this.props.mousetip;
+        const mouseTip = new MouseTip(settings);
+
+        mouseTip.start([this.card.current]);
+    }
+
+    componentDidMount() {
+        if (this.props.mousetip) this.mousetip();
+    }
+
+    render() {
+        const { icon, title, children, ...props } = this.props;
+
+        return (
+            <div className="card" {...props} ref={this.card}>
+                {this.createTitle(icon, title)}
+                <div className="card__description">{children}</div>
+            </div>
+        );
+    }
+}
 
 Card.propTypes = {
     icon: PropTypes.object,
     title: PropTypes.string,
+    mousetip: PropTypes.oneOfType([PropTypes.bool, PropTypes.object]),
     children: PropTypes.node.isRequired
+};
+
+Card.defaultProps = {
+    mousetip: false
 };
 
 export default Card;
--- src/pages/index.js
+++ src/pages/index.js
@@ -5,6 +5,7 @@ import {
 } from '@fortawesome/free-solid-svg-icons';
 import { faGithub } from '@fortawesome/free-brands-svg-icons';
 
+import Card from '../components/card';
 import Code from '../components/code';
 import ExternalLink from '../components/externallink';
 import Icon from '../components/icon';
@@ -54,6 +55,38 @@ const Summary = () => (
 const IndexPage = () => (
     <Layout summary={Summary}>
         <SEO title={title} description={[tagline, description].join(' ')} />
+        <h2>Examples</h2>
+        <div className="xs-h">
+            <div className="row">
+                <div className="col-4 mb-f">
+                    <Card mousetip={{ message: 'Test message!' }}>
+                        <span><strong>mousetip:message</strong>="Test message!"</span><br/>
+                        <span><strong>mt:m</strong>="Test message!"</span>
+                    </Card>
+                </div>
+                <div className="col-4 mb-f">
+                    <Card mousetip={{
+                        message: 'Another message!',
+                        direction: [ 'top', 'left' ]
+                    }}>
+                        <span><strong>mousetip:direction</strong>="top left"</span><br/>
+                        <span><strong>mt:d</strong>="top left"</span>
+                    </Card>
+                </div>
+                <div className="col-4 mb-f">
+                    <Card mousetip={{
+                        message: 'And another one!',
+                        styles: { backgroundColor: '#c31432' }
+                    }}>
+                        <span><strong>mousetip:background-color</strong>="#c31432"</span><br/>
+                        <span><strong>mt:bg</strong>="#c31432"</span>
+                    </Card>
+                </div>
+            </div>
+        </div>
+        <div className="xs-s">
+            <p>This library is intended for dekstop use only, as it requires a mouse cursor to work. Go check this site out on a desktop computer for a demo. Sorry for the inconvenience!</p>
+        </div>
         <h2>Installation</h2>
         <Code language="shell" example={false}>
             npm install purejs-mousetip
@@ -66,7 +99,7 @@ const IndexPage = () => (
                 <ul>
                     <li>The default styles have been tweaked to use more modern rules, such as <code>rem</code> instead of <code>px</code> for <code>padding</code>/<code>border-radius</code>, the use of <code>background-color</code> instead of <code>background</code>, and more.</li>
                     <li>Animations are here! They are enabled by default, but they can be turned off or changed as you see fit.</li>
-                    <li>Global styles are now stored within a <code>&lt;style&rt;</code> tag within the head of the page, created on <code>start()</code> and removed on <code>stop()</code>.</li>
+                    <li>Global styles are now stored within a <code>&lt;style&gt;</code> tag within the head of the page, created on <code>start()</code> and removed on <code>stop()</code>.</li>
                 </ul>
             </li>
             <li>
@@ -82,7 +115,7 @@ const IndexPage = () => (
                 <ul>
                     <li>Style attributes no longer need the <code>css-</code> prefix, and have been renamed to more closely resemble they're CSS rules.</li>
                     <li>A new <code>mousetip-style</code> attribute to alleviate the need for multiple style attributes or to style the mousetip locally beyond what's included.</li>
-                    <li>Shorthand variants are now a thing! Hate typing out <code>mousetip:background-color="..."</code>? Try <code>mt:bc="..."</code> instead.</li>
+                    <li>Shorthand variants are now a thing! Hate typing out <code>mousetip:background-color="..."</code>? Try <code>mt:bg="..."</code> instead.</li>
                 </ul>
             </li>
             <li>
